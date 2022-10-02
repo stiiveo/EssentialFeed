7d4382a0afacc8368f0faad043d87f1f1761c51f
Jason Ou <jason.ou.pro@gmail.com>
2022-10-02 16:29:14 +0800

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jason Ou on 2022/10/2.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
