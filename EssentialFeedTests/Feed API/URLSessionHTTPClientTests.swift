1ce1a7af72c2b3cc501ad079246f9050bd14da60
Jason Ou <jason.ou.pro@gmail.com>
2023-01-23 17:22:00 +0800

    Delivers empty data and response on successful HTTP response with `nil` data since the URL Loading System completes the request with a non-nil empty data value (0 bytes) which is a valid case (e.g. HTTP 204 no content response).

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -22,7 +22,7 @@ class URLSessionHTTPClient {
 			if let error = error {
 				completion(.failure(error))
 			}
-			else if let data = data, data.count > 0, let response = response as? HTTPURLResponse {
+			else if let data = data, let response = response as? HTTPURLResponse {
 				completion(.success(data, response))
 			}
 			else {
@@ -71,7 +71,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
 	func test_getFromURL_failsOnAllInvalidRepresentationCases() {
 		XCTAssertNotNil(resultError(for: nil, response: nil, error: nil))
 		XCTAssertNotNil(resultError(for: nil, response: nonHTTPURLResponse(), error: nil))
-		XCTAssertNotNil(resultError(for: nil, response: anyHTTPURLResponse(), error: nil))
 		XCTAssertNotNil(resultError(for: anyData(), response: nil, error: nil))
 		XCTAssertNotNil(resultError(for: anyData(), response: nil, error: anyNSError()))
 		XCTAssertNotNil(resultError(for: nil, response: anyHTTPURLResponse(), error: anyNSError()))
@@ -86,6 +85,29 @@ final class URLSessionHTTPClientTests: XCTestCase {
 		let result = resultSuccess(for: data, response: response)
 		XCTAssertNotNil(result.data)
 		XCTAssertNotNil(result.response)
+	}
+	
+	func test_getFromURL_succeedsWithEmptyDataOnHTTPURLResponseWithNilData() {
+		let response = anyHTTPURLResponse()
+		URLProtocolStub.stub(data: nil, response: response, error: nil)
+		
+		let exp = expectation(description: "Wait for load completion")
+		
+		makeSUT().get(from: anyURL()) { result in
+			switch result {
+			case let .success(receivedData, receivedResponse):
+				let emptyData = Data()
+				XCTAssertEqual(receivedData, emptyData)
+				XCTAssertEqual(receivedResponse.url, response.url)
+				XCTAssertEqual(receivedResponse.statusCode, response.statusCode)
+			default:
+				XCTFail("Expected success, got \(result) instead")
+			}
+			
+			exp.fulfill()
+		}
+		
+		wait(for: [exp], timeout: 1.0)
 	}
 	
 	// MARK: - Helpers
