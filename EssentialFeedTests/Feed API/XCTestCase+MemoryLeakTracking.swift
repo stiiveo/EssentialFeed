96c447fa08b4d40524bb81c53d9f8568d5f655a8
Jason Ou <jason.ou.pro@gmail.com>
2023-01-23 16:00:07 +0800

    Extract memory leak tracking helper method to a shared extension on XCTestCase

---
+++ EssentialFeedTests/Feed API/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,20 @@
+//
+//  XCTestCase+MemoryLeakTracking.swift
+//  EssentialFeedTests
+//
+//  Created by Jason Ou on 2023/1/23.
+//
+
+import XCTest
+
+extension XCTestCase {
+	func trackForMemoryLeaks(
+		_ instance: AnyObject,
+		file: StaticString = #filePath,
+		line: UInt = #line
+	) {
+		addTeardownBlock { [weak instance] in
+			XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+		}
+	}
+}
