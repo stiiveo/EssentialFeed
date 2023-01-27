6e779e5cf4a75d944b502a36f5951ef278beadc1
Jason Ou <jason.ou.pro@gmail.com>
2023-01-27 10:05:15 +0800

    Extract HTTPClientSpy and the helper method in `RemoteFeedLoaderTests` to separate files

---
+++ EssentialFeedTests/Feed API/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,38 @@
+//
+//  HTTPClientSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Jason Ou on 2023/1/27.
+//
+
+import EssentialFeed
+
+class HTTPClientSpy: HTTPClient {
+	private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
+	
+	var requestedURLs: [URL] {
+		messages.map { $0.url }
+	}
+	
+	var completions: [(HTTPClientResult) -> Void] {
+		messages.map { $0.completion }
+	}
+	
+	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+		messages.append((url, completion))
+	}
+	
+	func complete(with error: Error, at index: Int = 0) {
+		completions[index](.failure(error))
+	}
+	
+	func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+		let response = HTTPURLResponse(
+			url: requestedURLs[index],
+			statusCode: code,
+			httpVersion: nil,
+			headerFields: nil
+		)!
+		messages[index].completion(.success(data, response))
+	}
+}
