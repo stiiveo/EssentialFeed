eb43bae64dfb2d6ff9a48486a605b633bbe12931
Jason Ou <jason.ou.pro@gmail.com>
2023-01-31 23:34:22 +0800

    Move `LocalFeedLoader` and `FeedStore` collaborator to its own file in production

---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,46 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Jason Ou on 2023/1/31.
+//
+
+import Foundation
+
+public class LocalFeedLoader {
+	private let store: FeedStore
+	private let currentDate: () -> Date
+	
+	public init(store: FeedStore, currentDate: @escaping () -> Date) {
+		self.store = store
+		self.currentDate = currentDate
+	}
+	
+	public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+		store.deleteCachedFeed { [weak self] error in
+			guard let self = self else { return }
+			
+			if let cacheDeletionError = error {
+				completion(cacheDeletionError)
+			} else {
+				self.cache(items, with: completion)
+			}
+		}
+	}
+	
+	private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+		store.insert(items, timestamp: currentDate()) { [weak self] error in
+			guard self != nil else { return }
+			
+			completion(error)
+		}
+	}
+}
+
+public protocol FeedStore {
+	typealias DeletionCompletion = (Error?) -> Void
+	typealias InsertionCompletion = (Error?) -> Void
+	
+	func deleteCachedFeed(completion: @escaping DeletionCompletion)
+	func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
