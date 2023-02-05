ba815794b81084bb6bb0d60e8bb77b5e60a0aaa0
Jason Ou <jason.ou.pro@gmail.com>
2023-02-05 09:17:39 +0800

    Move `FeedCachePolicy` to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,25 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Jason Ou on 2023/2/5.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+	private init() {}
+	
+	private static let calendar = Calendar(identifier: .gregorian)
+	
+	private static var maxCacheAgeInDays: Int {
+		return 7
+	}
+	
+	static func validate(_ timestamp: Date, against date: Date) -> Bool {
+		guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+			return false
+		}
+		return date < maxCacheAge
+	}
+}
