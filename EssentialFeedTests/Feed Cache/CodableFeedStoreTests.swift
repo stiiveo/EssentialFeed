a856b07839484ae724c7e82fea28dbf72da9944b
Jason Ou <jason.ou.pro@gmail.com>
2023-02-06 16:31:38 +0800

    Extract duplicate delete test code into a reusable helper method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -171,15 +171,9 @@ class CodableFeedStoreTests: XCTestCase {
 	
 	func test_delete_hasNoSideEffectsOnEmptyCache() {
 		let sut = makeSUT()
-		let exp = expectation(description: "Wait for cache deletion")
+		let deletionError = deleteCache(from: sut)
 		
-		sut.deleteCachedFeed { deletionError in
-			XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-			exp.fulfill()
-		}
-		
-		wait(for: [exp], timeout: 1.0)
-		
+		XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
 		expect(sut, toRetrieve: .empty)
 	}
 	
@@ -187,12 +181,8 @@ class CodableFeedStoreTests: XCTestCase {
 		let sut = makeSUT()
 		insert((uniqueImageFeed().local, Date()), to: sut)
 		
-		let exp = expectation(description: "Wait for cache deletion")
-		sut.deleteCachedFeed { deletionError in
-			XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-			exp.fulfill()
-		}
-		wait(for: [exp], timeout: 1.0)
+		let deletionError = deleteCache(from: sut)
+		XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
 		
 		expect(sut, toRetrieve: .empty)
 	}
@@ -215,6 +205,18 @@ class CodableFeedStoreTests: XCTestCase {
 		}
 		wait(for: [exp], timeout: 1.0)
 		return insertionError
+	}
+	
+	@discardableResult
+	private func deleteCache(from sut: CodableFeedStore) -> Error? {
+		let exp = expectation(description: "Wait for cache deletion")
+		var deletionError: Error?
+		sut.deleteCachedFeed { receivedDeletionError in
+			deletionError = receivedDeletionError
+			exp.fulfill()
+		}
+		wait(for: [exp], timeout: 1.0)
+		return deletionError
 	}
 	
 	private func expect(_ sut: CodableFeedStore, toRetrieve expectedResult: RetrieveCachedFeedResult, file: StaticString = #filePath, line: UInt = #line) {
