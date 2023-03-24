3e1af45f5063fd4ff3081e9c1807ecba7d015a0f
Jason Ou <jason.ou.pro@gmail.com>
2023-03-24 17:55:16 +0800

    `LocalFeedLoader.validateCache` succeeds on empty cache

--- EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -100,6 +100,14 @@ class ValidateFeedCacheUseCaseTests: XCTestCase {
 		
 		XCTAssertEqual(store.receivedMessages, [.retrieve])
 	}
+    
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrievalWithEmptyCache()
+        })
+    }
 
 	// MARK: - Helper
 	
