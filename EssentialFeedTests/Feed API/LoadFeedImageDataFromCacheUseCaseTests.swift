58352383609867a25489a2bb6f7eaa7bb3041c38
Jason Ou <jason.ou.pro@gmail.com>
2023-03-24 11:40:52 +0800

    Rename test case class to clarify intent

--- EssentialFeedTests/Feed API/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  LocalFeedImageDataLoaderTests.swift
+//  LoadFeedImageDataFromCacheUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by JasonOu on 2023/3/23.
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class LocalFeedImageDataLoaderTests: XCTestCase {
+class LoadFeedImageDataFromCacheUseCaseTests: XCTestCase {
     
     func test_init_doesNotMessageStoreUponCreation() {
         let (_, store) = makeSUT()
