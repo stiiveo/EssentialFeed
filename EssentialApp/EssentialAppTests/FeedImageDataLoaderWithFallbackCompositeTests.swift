370d571ad820f10ad666e14209c8e847ba4f4b38
Jason Ou <jason.ou.pro@gmail.com>
2023-03-25 17:41:32 +0800

    `FeedImageDataLoaderWithFallbackComposite.init` does not load image data

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,60 @@
+//
+//  FeedImageLoaderWithFallbackCompositeTests.swift
+//  EssentialAppTests
+//
+//  Created by Jason Ou on 2023/3/25.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageLoaderWithFallbackComposite: FeedImageDataLoader {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {
+            
+        }
+    }
+    
+    init(primary: FeedImageDataLoader, fallback: FeedImageDataLoader) {
+
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return Task()
+    }
+}
+
+class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    
+    func test_init_doesNotLoadImageData() {
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        _ = FeedImageLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        
+        XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+    
+    // MARK: - Helpers
+    
+    private func anyURL() -> URL {
+        return URL(string: "http://any-url.com")!
+    }
+    
+    private class LoaderSpy: FeedImageDataLoader {
+        private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()
+        
+        var loadedURLs: [URL] {
+            return messages.map { $0.url }
+        }
+        
+        private struct Task: FeedImageDataLoaderTask {
+            func cancel() {}
+        }
+        
+        func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+            messages.append((url, completion))
+            return Task()
+        }
+    }
+}
