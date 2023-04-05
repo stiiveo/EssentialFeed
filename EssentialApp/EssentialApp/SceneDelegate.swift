58a2d31d960a4f823811507704015a25005b2317
Jason Ou <jason.ou.pro@gmail.com>
2023-04-05 11:17:13 +0800

    Configures feed navigation as window root view controller

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -19,18 +19,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
-        
+    
+        configureWindow()
+    }
+    
+    func configureWindow() {
         let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
         
         let remoteClient = makeRemoteClient()
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
-                
+        
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)
-                
-        window?.rootViewController = FeedUIComposer.feedComposedWith(
+        
+        window?.rootViewController = UINavigationController(rootViewController: FeedUIComposer.feedComposedWith(
             feedLoader: FeedLoaderWithFallbackComposite(
                 primary: FeedLoaderCacheDecorator(
                     decoratee: remoteFeedLoader,
@@ -40,7 +44,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 primary: FeedImageLoaderCacheDecorator(
                     decoratee: remoteImageLoader,
                     cache: localImageLoader),
-                fallback: localImageLoader))
+                fallback: localImageLoader)))
     }
     
     func makeRemoteClient() -> HTTPClient {
