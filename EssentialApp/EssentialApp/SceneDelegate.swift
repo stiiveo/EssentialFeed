7eca7edf56c7166aced905cd89d1faf51c9d941a
Jason Ou <jason.ou.pro@gmail.com>
2023-08-21 17:39:38 +0800

    Make feed loader closure return Paginated<FeedImage>

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -79,13 +79,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
     
-    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<[FeedImage], Error> {
+    private func makeRemoteFeedLoaderWithLocalFallback() -> AnyPublisher<Paginated<FeedImage>, Error> {
         let url = FeedEndpoint.get.url(baseURL: baseURL)
         return remoteFeedLoader
             .getPublisher(url: url)
             .tryMap(FeedItemMapper.map)
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
+            .map {
+                Paginated(items: $0)
+            }
+            .eraseToAnyPublisher()
     }
     
     private func makeLocalImageLoaderWithRemoteFallback(url: URL) -> FeedImageDataLoader.Publisher {
---
+++ EssentialFeed/EssentialFeed/Shared API/Paginated.swift
@@ -0,0 +1,20 @@
+//
+//  Paginated.swift
+//  EssentialFeed
+//
+//  Created by JasonOu on 2023/8/21.
+//
+
+import Foundation
+
+public struct Paginated<Item> {
+    public typealias LoadMoreCompletion = (Result<Paginated<Item>, Error>) -> Void
+    
+    public let items: [Item]
+    public let loadMore: ((@escaping LoadMoreCompletion) -> Void)?
+    
+    public init(items: [Item], loadMore: ((LoadMoreCompletion) -> Void)? = nil) {
+        self.items = items
+        self.loadMore = loadMore
+    }
+}
