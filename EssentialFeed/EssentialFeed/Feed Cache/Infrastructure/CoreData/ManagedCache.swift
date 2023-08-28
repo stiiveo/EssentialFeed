3170bf90aea3f478c99a50d3cb452d848fa25652
Jason Ou <jason.ou.pro@gmail.com>
2023-08-28 15:52:05 +0800

    Remove duplication

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -21,9 +21,13 @@ extension ManagedCache {
 	}
 	
 	static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
-		try find(in: context).map(context.delete)
+        try deleteCache(in: context)
 		return ManagedCache(context: context)
 	}
+    
+    static func deleteCache(in context: NSManagedObjectContext) throws {
+        try find(in: context).map(context.delete)
+    }
 	
 	var localFeed: [LocalFeedImage] {
 		return feed.compactMap {
