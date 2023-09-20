ba5e2e7f630c30ef0c03a5aa94af98aeb82e2fa8
Jason Ou <jason.ou.pro@gmail.com>
2023-09-20 16:18:10 +0800

    Remove unused async methods

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -87,33 +87,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
 		
 		expect(sut, toRetrieve: .success(.none), file: file, line: line)
 	}
-	
-	func assertThatSideEffectsRunSerially(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
-		var completedOperationsInOrder = [XCTestExpectation]()
-		
-		let op1 = expectation(description: "Operation 1")
-		sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
-			completedOperationsInOrder.append(op1)
-			op1.fulfill()
-		}
-		
-		let op2 = expectation(description: "Operation 2")
-		sut.deleteCachedFeed { _ in
-			completedOperationsInOrder.append(op2)
-			op2.fulfill()
-		}
-		
-		let op3 = expectation(description: "Operation 3")
-		sut.insert(uniqueImageFeed().local, timestamp: Date()) { _ in
-			completedOperationsInOrder.append(op3)
-			op3.fulfill()
-		}
-		
-		waitForExpectations(timeout: 5.0)
-		
-		XCTAssertEqual(completedOperationsInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order", file: file, line: line)
-	}
-	
+
 }
 
 extension FeedStoreSpecs where Self: XCTestCase {
@@ -138,7 +112,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
         }
 	}
 	
-    func expect(_ sut: FeedStore, toRetrieve expectedResult: FeedStore.RetrievalResult, file: StaticString = #filePath, line: UInt = #line) {
+    func expect(_ sut: FeedStore, toRetrieve expectedResult: Result<CachedFeed?, Error>, file: StaticString = #filePath, line: UInt = #line) {
         let retrievedResult = Result { try sut.retrieve() }
         
         switch (expectedResult, retrievedResult) {
@@ -155,7 +129,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
         }
 	}
 	
-	func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: FeedStore.RetrievalResult, file: StaticString = #filePath, line: UInt = #line) {
+	func expect(_ sut: FeedStore, toRetrieveTwice expectedResult: Result<CachedFeed?, Error>, file: StaticString = #filePath, line: UInt = #line) {
 		expect(sut, toRetrieve: expectedResult)
 		expect(sut, toRetrieve: expectedResult)
 	}
